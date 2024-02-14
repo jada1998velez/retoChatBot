2f3b1cee1427197ec51d2e5d8dda424e7fc37249
José Antonio Díaz <146923715+jada1998velez@users.noreply.github.com>
2024-02-14 11:15:16 +0100

    Update app.py

--- src/app.py
+++ src/app.py
@@ -11,7 +11,6 @@ PINECONE_API_KEY = st.secrets["PINECONE_API_KEY_V2"]
 PINECONE_ENV = st.secrets["PINECONE_ENV"]
 PINECONE_INDEX_NAME = st.secrets["PINECONE_INDEX_NAME"]
 
-pc = Pinecone(api_key=PINECONE_API_KEY)
 
 api_key = st.secrets["OPENAI_API_KEY"]
 api_base = st.secrets["OPENAI_API_BASE"]
@@ -27,10 +26,10 @@ openai.api_type = api_type
 EMBEDDING_MODEL = "text-embedding-ada-002"
 GPT_EMBEDDING_ENGINE = 'mondongodb'
 DIMENSION = 1536
-# GPT_MODEL = 'gpt-3.5-turbo-16k'
-# GPT_CHAT_ENGINE = "gepeto"
-GPT_MODEL = 'gpt-4'
-GPT_CHAT_ENGINE = "dictador"
+GPT_MODEL = 'gpt-3.5-turbo-16k'
+GPT_CHAT_ENGINE = "gepeto"
+#GPT_MODEL = 'gpt-4'
+# GPT_CHAT_ENGINE = "dictador"
 
 
 st.title("Chatbot")
@@ -40,32 +39,32 @@ uploaded_file = st.file_uploader("Sube tu archivo PDF", type="pdf")
 if uploaded_file is not None:
     pdf_bytes = uploaded_file.read()
     pdf_file = BytesIO(pdf_bytes)
-    docsearch = pdf_gpt.process_pdf(pdf_bytes,api_key,PINECONE_API_KEY,PINECONE_ENV,PINECONE_INDEX_NAME)
+    docsearch = pdf_gpt.process_pdf(pdf_file,api_key,PINECONE_API_KEY,PINECONE_ENV,PINECONE_INDEX_NAME, DIMENSION)
     st.success("¡Archivo PDF procesado exitosamente!")
     pdf_file.close()
 
 if "messages" not in st.session_state:
   st.session_state["messages"] = [{"role": "assistant", "content": "Hola, soy ChatGPT, ¿En qué puedo ayudarte?"}]
+else:
+  for msg in st.session_state["messages"]:
+    st.chat_message(msg["role"]).write(msg["content"])
 
-for msg in st.session_state["messages"]:
-  st.chat_message(msg["role"]).write(msg["content"])
+  if user_input := st.chat_input():
+    st.session_state["messages"].append({"role": "user", "content": user_input})
+    st.chat_message("user").write(user_input)
 
-if user_input := st.chat_input():
-  st.session_state["messages"].append({"role": "user", "content": user_input})
-  st.chat_message("user").write(user_input)
+    if is_pdf_chatbot and uploaded_file is not None:
+      responseMessage = pdf_gpt.get_answer(docsearch,user_input,api_key)
+    else:
+      response = openai.ChatCompletion.create(
+          model=GPT_MODEL,
+          messages=st.session_state["messages"],
+          engine=GPT_CHAT_ENGINE,
+          max_tokens=DIMENSION
+      )
+      responseMessage = response['choices'][0]['message']['content']
+      st.session_state["messages"].append({"role": "assistant", "content": responseMessage})
+      st.chat_message("assistant").write(responseMessage)
+  
 
-  if is_pdf_chatbot and uploaded_file is not None:
-    responseMessage = pdf_gpt.get_answer(docsearch,user_input,api_key)
-  else:
-    response = openai.ChatCompletion.create(
-        model=GPT_MODEL,
-        messages=st.session_state["messages"],
-        engine=GPT_CHAT_ENGINE,
-        max_tokens=DIMENSION
-    )
-    responseMessage = response['choices'][0]['message']['content']
-
-  st.session_state["messages"].append({"role": "assistant", "content": responseMessage})
-  st.chat_message("assistant").write(responseMessage)
-
-#pinecone.deinit()  
+ 
